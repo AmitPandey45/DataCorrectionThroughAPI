6989cad66c5fdd5f5fe0e2166569c618d4eeeaff
Amit Pandey <amit.p@icreon.com>
2022-05-16 19:33:36 +0530

    Done the code changes to load data from excel file using a free library

--- MemberAndOrganizationDataCorrectionInEBS/Program.cs
+++ MemberAndOrganizationDataCorrectionInEBS/Program.cs
@@ -1,11 +1,16 @@
-﻿using IronXL;
+﻿using ExcelDataReader;
+using IronXL;
 using MemberAndOrganizationDataCorrectionInEBS.Extension;
 using MemberAndOrganizationDataCorrectionInEBS.Interface;
 using MemberAndOrganizationDataCorrectionInEBS.Model;
 using MemberAndOrganizationDataCorrectionInEBS.Utility;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 
 namespace MemberAndOrganizationDataCorrectionInEBS
@@ -33,50 +38,73 @@ namespace MemberAndOrganizationDataCorrectionInEBS
             var logger = program.memberSystemLogger;
             var externalService = program.externalService;
 
-            ////List<MemberOrOrganizationAccountRelationshipDetailsDto> activeDataAfter31March2022 =
-            ////    GetAllMemberAssociatedWithAnOrganization(externalService);
-
-            ////CreateExcelSheetForAllActiveMembersAfter30March2022(activeDataAfter31March2022);
-
             ////return;
-
-            List<MemberAndOrganizationAccountDetail> allMemberAndOrgAccountDetails =
-                LoadMemberAndOrganizationAccountDetailFromExel();
-
-            var testingWithOnlyTwoDataSet = allMemberAndOrgAccountDetails.Where(s => !string.IsNullOrEmpty(s.OrgAccountNumber)).ToList();
-
-            logger.LogTrace("=================MemberAndOrganizationDataCorrectionInEBS Process Start=================");
-
-            foreach (var memberAndOrgDetails in testingWithOnlyTwoDataSet)
+            try
             {
-                logger.LogTrace($"******************Start the data correction for Member Account- {memberAndOrgDetails.MemberAccountNumber} Member ContactId- {memberAndOrgDetails.MemberContactId} Previous Organization Account- {OrganizationAccountNumber} Correct Organization Account- {memberAndOrgDetails.OrgAccountNumber}******************");
+                ////List<MemberOrOrganizationAccountRelationshipDetailsDto> activeDataAfter31March2022 =
+                ////    GetAllMemberAssociatedWithAnOrganization(externalService);
 
-                //// This Mule API will break the relationship with Member Account & Fixed Org Account- 000113065 in EBS System
-                if (!string.IsNullOrEmpty(OrganizationAccountNumber) && memberAndOrgDetails.MemberContactId > 0)
+                ////CreateExcelSheetForAllActiveMembersAfter30March2022(activeDataAfter31March2022);
+
+                ////return;
+
+                List<MemberAndOrganizationAccountDetail> allMemberAndOrgAccountDetails =
+                    LoadMemberAndOrganizationAccountDetailFromExelWithFreeLibrary(logger);
+
+               //// List<MemberAndOrganizationAccountDetail> allMemberAndOrgAccountDetails1 =
+               ////     LoadMemberAndOrganizationAccountDetailFromExel();
+
+               //// allMemberAndOrgAccountDetails1.RemoveAt(174);
+
+               ////var diff =
+               ////     allMemberAndOrgAccountDetails
+               ////     .Where(s => !allMemberAndOrgAccountDetails1
+               ////     .Any(t => s.MemberAccountNumber.Equals(t.MemberAccountNumber) &&
+               ////     s.MemberContactId.Equals(t.MemberContactId) &&
+               ////     s.OrgAccountNumber.Equals(t.OrgAccountNumber))
+               ////     );
+
+                var testingWithOnlyTwoDataSet = allMemberAndOrgAccountDetails.Where(s => !string.IsNullOrEmpty(s.OrgAccountNumber)).ToList();
+                testingWithOnlyTwoDataSet = allMemberAndOrgAccountDetails;
+                ////Console.Read();
+                ////return;
+                logger.LogTrace("=================MemberAndOrganizationDataCorrectionInEBS Process Start=================");
+
+                foreach (var memberAndOrgDetails in testingWithOnlyTwoDataSet)
                 {
-                    InActivateOrgAndMemberRelationInEBSSystem(externalService, memberAndOrgDetails);
-                }
-                //// This Mule API will create the relationship with Member Account & Correct Organization Account
-                if (!string.IsNullOrEmpty(memberAndOrgDetails.OrgAccountNumber) && !string.IsNullOrEmpty(memberAndOrgDetails.MemberAccountNumber))
-                {
-                    externalService.EBSOrganizationMemberRelation(memberAndOrgDetails.OrgAccountNumber, memberAndOrgDetails.MemberAccountNumber);
+                    logger.LogTrace($"******************Start the data correction for Member Account- {memberAndOrgDetails.MemberAccountNumber} Member ContactId- {memberAndOrgDetails.MemberContactId} Previous Organization Account- {OrganizationAccountNumber} Correct Organization Account- {memberAndOrgDetails.OrgAccountNumber}******************");
+
+                    //// This Mule API will break the relationship with Member Account & Fixed Org Account- 000113065 in EBS System
+                    if (!string.IsNullOrEmpty(OrganizationAccountNumber) && memberAndOrgDetails.MemberContactId > 0)
+                    {
+                        InActivateOrgAndMemberRelationInEBSSystem(externalService, memberAndOrgDetails);
+                    }
+                    //// This Mule API will create the relationship with Member Account & Correct Organization Account
+                    if (!string.IsNullOrEmpty(memberAndOrgDetails.OrgAccountNumber) && !string.IsNullOrEmpty(memberAndOrgDetails.MemberAccountNumber))
+                    {
+                        externalService.EBSOrganizationMemberRelation(memberAndOrgDetails.OrgAccountNumber, memberAndOrgDetails.MemberAccountNumber);
+                    }
+
+                    logger.LogTrace($"******************End the data correction for Member Account- {memberAndOrgDetails.MemberAccountNumber} Member ContactId- {memberAndOrgDetails.MemberContactId} Previous Organization Account- {OrganizationAccountNumber} Correct Organization Account- {memberAndOrgDetails.OrgAccountNumber}******************");
                 }
 
-                logger.LogTrace($"******************End the data correction for Member Account- {memberAndOrgDetails.MemberAccountNumber} Member ContactId- {memberAndOrgDetails.MemberContactId} Previous Organization Account- {OrganizationAccountNumber} Correct Organization Account- {memberAndOrgDetails.OrgAccountNumber}******************");
+                logger.LogTrace("=================MemberAndOrganizationDataCorrectionInEBS Process End=================");
+
+                ////return;
+
+                ////logger.LogTrace("Program execuation start");
+
+                ////List<MemberOrOrganizationAccountRelationshipDetailsDto> activeDataAfter31March2022 =
+                ////    GetAllMemberAssociatedWithAnOrganization(externalService);
+
+                ////CreateExcelSheetForAllActiveMembersAfter30March2022(activeDataAfter31March2022);
+
+                ////logger.LogTrace("Program execuation end");
             }
-
-            logger.LogTrace("=================MemberAndOrganizationDataCorrectionInEBS Process End=================");
-
-            ////return;
-
-            ////logger.LogTrace("Program execuation start");
-
-            ////List<MemberOrOrganizationAccountRelationshipDetailsDto> activeDataAfter31March2022 =
-            ////    GetAllMemberAssociatedWithAnOrganization(externalService);
-
-            ////CreateExcelSheetForAllActiveMembersAfter30March2022(activeDataAfter31March2022);
-
-            ////logger.LogTrace("Program execuation end");
+            catch(Exception ex)
+            {
+                logger.LogException(ex);
+            }
         }
 
         private static List<MemberOrOrganizationAccountRelationshipDetailsDto> GetAllMemberAssociatedWithAnOrganization(IExternalService externalService)
@@ -94,7 +122,7 @@ namespace MemberAndOrganizationDataCorrectionInEBS
                 .Where(m => m.Start_Date >= march31_2022 && m.RelationshipStatus.Equals("A")).ToList();
             var inActiveDataAfter31March2022 = allMemberAssociatedWithOrg
                 .Where(m => m.Start_Date >= march31_2022 && m.RelationshipStatus.Equals("I")).ToList();
-            return activeDataAfter31March2022;
+            return activeData;
         }
 
         private static List<MemberAndOrganizationAccountDetail> LoadMemberAndOrganizationAccountDetailFromExel()
@@ -117,6 +145,45 @@ namespace MemberAndOrganizationDataCorrectionInEBS
                             MemberContactId = !string.IsNullOrEmpty(dtWorkSheet.Rows[i][1].ToString()) ? Convert.ToInt64(dtWorkSheet.Rows[i][1].ToString()) : 0,
                             OrgAccountNumber = dtWorkSheet.Rows[i][2].ToString(),
                         });
+                }
+            }
+
+            return allMemberAndOrgAccountDetails;
+        }
+
+        private static List<MemberAndOrganizationAccountDetail> LoadMemberAndOrganizationAccountDetailFromExelWithFreeLibrary(IMemberSystemLoggerService logger)
+        {
+            var allMemberAndOrgAccountDetails = new List<MemberAndOrganizationAccountDetail>();
+            int iterator = 0;
+            using (var stream = File.Open(FileName, FileMode.Open, FileAccess.Read))
+            {
+                using (var reader = ExcelReaderFactory.CreateReader(stream))
+                {
+                    do
+                    {
+                        while (reader.Read())
+                        {
+                            if (iterator > 0)
+                            {
+                                var memberAndOrgAccountDetails = new MemberAndOrganizationAccountDetail();
+
+                                try
+                                {
+                                    memberAndOrgAccountDetails.MemberAccountNumber = Convert.ToString(reader.GetValue(0));
+                                    memberAndOrgAccountDetails.MemberContactId = !string.IsNullOrEmpty(Convert.ToString(reader.GetValue(1))) ? Convert.ToInt64(Convert.ToString(reader.GetValue(1))) : 0;
+                                    memberAndOrgAccountDetails.OrgAccountNumber = Convert.ToString(reader.GetValue(2));
+                                }
+                                catch(Exception ex)
+                                {
+                                    logger.LogException(ex);
+                                }
+
+                                allMemberAndOrgAccountDetails.Add(memberAndOrgAccountDetails);
+                            }
+
+                            iterator++;
+                        }
+                    } while (reader.NextResult());
                 }
             }
 
